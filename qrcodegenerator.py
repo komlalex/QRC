c7542f655836ba96370f81a1b02bbe195c1ef64a
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-21 14:43:25 +0000

    Final commit

--- qrcodegenerator.py
+++ qrcodegenerator.py
@@ -1,3 +1,26 @@
 import qrcode
-import image
 
+
+
+
+
+def generate_qrcode(url): 
+    qr = qrcode.QRCode(
+    version = 15,
+    error_correction = qrcode.constants.ERROR_CORRECT_L,
+    box_size = 10,
+    border = 5
+)
+
+
+
+    qr.add_data(url)
+
+    qr.make(fit = True)
+
+    img = qr.make_image(fill = "black", back_color = "white")
+    img.save("myqrcimage.png")
+
+
+url = input("Enter URL: ")
+generate_qrcode(url)
